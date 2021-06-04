bdedfd20ba464b0b6edcc7ba2e477e52d0203390
matildaz <irandom.com@mail.ru>
2021-06-04 15:44:37 +0300

    Added some classes

--- the_game/mobs.hpp
+++ the_game/mobs.hpp
@@ -2,6 +2,30 @@
 //random_shuffle(arr[0],arr[3]);
 
 class lightMobs {
-    int healthPoints;    
-    
+    int healthPoints = 10;    
+    int coins = 5;
+
+    int levelUp() {
+        healthPoints += 10;
+        coins += 5;
+    }
+};
+
+class boss {
+    int healthPoints = 100;
+    int coins = 100;
+    int timeToLeave = 90;
+
+    int levelUp() {
+        healthPoints += 100;
+        coins += 100;
+    }
+};
+
+class player {
+    int damage = 1;
+
+    int swordUpgrade() {
+        damage += 1;
+    }
 };
