511b82e3b3a94d2b0cf16effa79decf9b735768b
matildaz <irandom.com@mail.ru>
2021-06-04 13:14:21 +0300

    CMake - done, but have some problems with starting the progect

---
+++ out/build/x64-Release/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/asis/Desktop/Cpp Project
+# Build directory: C:/Users/asis/Desktop/Cpp Project/out/build/x64-Release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/doctest")
